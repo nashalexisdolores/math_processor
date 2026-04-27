d4dcfedec029c0a089e064bf507a56c8894d4657
nashalexisdolores <alxsdlrs@gmail.com>
2026-04-28 00:55:27 +0800

    implement square/cube logic for even/odd integers

--- math_processor.py
+++ math_processor.py
@@ -1,3 +1,14 @@
 class MathProcessor:
     def __init__(self, input_f="integers.txt"):
         self.input_f = input_f
+
+    def run_math(self):
+        with open(self.input_f, 'r') as src, \
+             open('double.txt', 'w') as d, \
+             open('triple.txt', 'w') as t:
+            for line in src:
+                n = int(line.strip())
+                if n % 2 == 0:
+                    d.write(f"{n**2}\n")
+                else:
+                    t.write(f"{n**3}\n")
